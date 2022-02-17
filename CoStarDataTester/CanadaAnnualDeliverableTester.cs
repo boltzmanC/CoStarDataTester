70bb4ef228760a6c7d31dbf1d825bbe4882912f0
Dylan White <Dylan.White@team.neustar>
2022-02-17 10:00:08 -0800

    Import FluentFTP nugetpackage

--- CoStarDataTester/CanadaAnnualDeliverableTester.cs
+++ CoStarDataTester/CanadaAnnualDeliverableTester.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Globalization;
+using System.Data;
+using FluentFTP;
 
 namespace CoStarDataTester
 {
